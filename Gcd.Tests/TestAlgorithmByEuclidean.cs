6e574265701df489500a7239e305602e5874bd33
AlexanderKarcheuski <karchevskji@gmail.com>
2022-01-15 12:52:31 +0300

    Created methods for test algorithm euclidean with two parameters and throws

---
+++ Gcd.Tests/TestAlgorithmByEuclidean.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using System;
+
+namespace Gcd.Tests
+{
+    public class TestAlgorithmByEuclidean
+    {
+        private TimeSpan spendTime;
+
+        [TestCase(1, 1, ExpectedResult = 1)]
+        [TestCase(10, 14, ExpectedResult = 2)]
+        [TestCase(55, 70, ExpectedResult = 5)]
+        [TestCase(1400, 2222, ExpectedResult = 2)]
+        [TestCase(20, 44, ExpectedResult = 4)]
+        [TestCase(100, 16, ExpectedResult = 4)]
+        [TestCase(96, 16, ExpectedResult = 16)]
+        [TestCase(-20, 44, ExpectedResult = 4)]
+        [TestCase(-100, -16, ExpectedResult = 4)]
+        [TestCase(96, -16, ExpectedResult = 16)]
+        public int GetGcdWithTwoParameters(int a, int b)
+        {
+            return GCD.GetGcdByEuclidean(a, b, out spendTime);
+        }
+
+        [Test]
+        public void GetGcdWithTwoParameters_ThrowArgumentException() => Assert.Throws<ArgumentException>(() => GCD.GetGcdByEuclidean(0, 0), "Throwed argument exception because method containt zero argument");
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine($"Method:{this.GetType()}\nSpend time:{this.spendTime}");
+        }
+    }
+}
